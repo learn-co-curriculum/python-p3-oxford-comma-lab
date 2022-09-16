9d94c6addbb920d6547672234723f8df97d5e672
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-16 10:44:00 -0500

    add solution

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) ==2:
+        return ' and '.join(items)
+    else:
+        return ', '.join(items[:-1]) + f', and {items[-1]}'
+    
